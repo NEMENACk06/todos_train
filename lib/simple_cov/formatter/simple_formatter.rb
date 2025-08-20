c66735b43786b50aa22edfc5b4112d25b68a48b3
nemenack <nisit9733@gmail.com>
2025-08-20 13:17:04 +0700

    CI: add test minitest to pipeline

--- lib/simple_cov/formatter/simple_formatter.rb
+++ lib/simple_cov/formatter/simple_formatter.rb
@@ -3,10 +3,10 @@ module SimpleCov
   module Formatter
     class SimpleFormatter
       def format(result)
-        percent = format('%.2f', result.covered_percent)
+        percent = format("%.2f", result.covered_percent)
         puts "Coverage: #{percent}%"
-        FileUtils.mkdir_p(Rails.root.join('coverage'))
-        File.write(Rails.root.join('coverage', 'summary.txt'), "Coverage: #{percent}%\n")
+        FileUtils.mkdir_p(Rails.root.join("coverage"))
+        File.write(Rails.root.join("coverage", "summary.txt"), "Coverage: #{percent}%\n")
       end
     end
   end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,9 @@
 # test/test_helper.rb หรือ spec/spec_helper.rb ก่อน SimpleCov.start
-require 'simplecov'
-require Rails.root.join('lib/simple_cov/formatter/simple_formatter')
+require "simplecov"
+require Rails.root.join("lib/simple_cov/formatter/simple_formatter")
 
 SimpleCov.formatters = [
   SimpleCov::Formatter::HTMLFormatter,
   SimpleCov::Formatter::SimpleFormatter
 ]
-SimpleCov.start 'rails'
+SimpleCov.start "rails"
